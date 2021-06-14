1a3654cf21feeee2d1a52de1f4806e7dc8f520c7
mathieugery <mathieu.gery@epitech.eu>
2021-06-15 01:59:13 +0200

    [+] Dashboard page with all informations

--- src/components/statsWidget/index.js
+++ src/components/statsWidget/index.js
@@ -1,7 +1,19 @@
 export default function StatsWidget(props) {
     return (
-        <div>
-            <h3 className="text-lg leading-6 font-medium text-gray-900">Last 30 days</h3>
+        <div className="mt-6">
+
+            <div className="relative">
+                <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                    <div className="w-full border-t border-gray-300" />
+                </div>
+                <div className="relative flex justify-start">
+                    {props.first === "true" ? (<span className="pr-3 text-lg bg-gray-800 font-medium text-white">{props.title}</span>
+                    ) : (
+                        <span className="pr-3 text-lg bg-white font-medium text-gray-800">{props.title}</span>
+                    )}
+                </div>
+            </div>
+
             <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
                 {props.statsList.map((item) => (
                     <div key={item.name} className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,18 +2,64 @@ import NavBar from '../../components/navBar'
 import PageContent from "../../components/pageContent";
 import StatsWidget from "../../components/statsWidget";
 
+import {toast} from "react-toastify";
+import handleAxiosResponseError from "../../helpers/handleAxiosResponseError";
+import statsUsers from "../../api/stats/users";
+import statsAnnounces from "../../api/stats/announces";
+import {useState} from "react";
+import {useEffect} from "react";
+
 export default function Dashboard() {
+    const [statsUsersArray, setStatsUsersArray] = useState([]);
+    const [statsAnnouncesArray, setStatsAnnouncesArray] = useState([]);
+
+    const getMetrics = (event) => {
+        statsUsers().then((response) => {
+            if (response.success) {
+                setStatsUsersArray(response.success);
+            }
+            else if (response.warning)
+                toast.warning(response.warning);
+        }).catch((error) => {
+            toast.error(handleAxiosResponseError(error));
+        });
+        statsAnnounces().then((response) => {
+            if (response.success) {
+                setStatsAnnouncesArray(response.success);
+            }
+            else if (response.warning)
+                toast.warning(response.warning);
+        }).catch((error) => {
+            toast.error(handleAxiosResponseError(error));
+        });
+    }
+
+    useEffect(() => {
+        getMetrics();
+        console.log(statsAnnouncesArray)
+    }, []);
+
     return (
         <div>
             <NavBar/>
             <PageContent content={
-                <StatsWidget statsList={[
-                    { name: 'Total Subscribers', stat: '71,897' },
-                    { name: 'Avg. Open Rate', stat: '58.16%' },
-                    { name: 'Avg. Click Rate', stat: '24.57%' },
-                    { name: 'Avg. Open Rate', stat: '58.16%' },
+                <div><StatsWidget first="true" title="Utilisateurs" statsList={[
+                    { name: 'Total Users', stat: statsUsersArray.users_total },
+                    { name: 'Registered Users', stat: statsUsersArray.registered_users },
+                    { name: 'Confirmed Users', stat: statsUsersArray.confirmed_users }
                 ]}/>
+                <StatsWidget title="Announces" statsList={[
+                    { name: 'Total Announces', stat: statsAnnouncesArray.announces_total },
+                    { name: 'Available Announces', stat: statsAnnouncesArray.available_announces },
+                    { name: 'WFR Announces', stat: statsAnnouncesArray.waiting_for_reservation_announces },
+                    { name: 'Given Announces', stat: statsAnnouncesArray.given_announces },
+                    { name: 'Noted Annonces', stat: statsAnnouncesArray.noted_announces },
+                    { name: 'Deleted Announces', stat: statsAnnouncesArray.deleted_announces }
+                ]}/>
+                </div>
+
             }/>
+
         </div>
     )
 }
