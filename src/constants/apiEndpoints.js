fa516ffaadb25970f5644d79670e1b0de6eed541
mathieugery <mathieu.gery@epitech.eu>
2021-06-14 19:22:03 +0200

    [+] Api call get announces

--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -2,3 +2,4 @@ const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const SIGN_IN_URL = BASE_URL + '/auth/login';
 export const LIST_USERS_URL = BASE_URL + '/users/list';
+export const LIST_ANNOUNCES_URL = BASE_URL + '/products/list';
